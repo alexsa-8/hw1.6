6daabb421d296a422dad7553d1fc3fd7bebf6971
Alex <alexsa-8@mail.ru>
2022-08-17 11:46:20 +0300

    в 4 задание я вывел содержение массива задам на перёд, так правельно? Или надо в массиве поменять расположения заначений и потом вывести начиная с нулевого индекса и заканчивая последним?

--- src/Main.java
+++ src/Main.java
@@ -42,15 +42,8 @@ public class Main {
         //Задание 4
         System.out.println("\nЗадание 4");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        char a;
-        int n = reverseFullName.length;
-        for (int i = 0; i < n / 2; i++) {
-            a = reverseFullName[n - i - 1];
-            reverseFullName[n - i - 1] = reverseFullName[i];
-            reverseFullName[i] = a;
-        }
-        for (char element:reverseFullName){
-            System.out.print(element);
+        for (int i=reverseFullName.length-1; i >=0 ; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
 }
