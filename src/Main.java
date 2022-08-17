58381828f1149a08f808ca0764fa2232bf5de939
Alex <alexsa-8@mail.ru>
2022-08-17 12:06:50 +0300

    2 варианта

--- src/Main.java
+++ src/Main.java
@@ -42,8 +42,18 @@ public class Main {
         //Задание 4
         System.out.println("\nЗадание 4");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i=reverseFullName.length-1; i >=0 ; i--) {
-            System.out.print(reverseFullName[i]);
+        char a;
+        int n = reverseFullName.length;
+        for (int i = 0; i < n / 2; i++) {
+            a = reverseFullName[n - i - 1];
+            reverseFullName[n - i - 1] = reverseFullName[i];
+            reverseFullName[i] = a;
         }
+        for (char element:reverseFullName){
+            System.out.print(element);
+        }
+        // for (int i=reverseFullName.length-1; i >=0 ; i--) {
+        //     System.out.print(reverseFullName[i]);
+        // }
     }
 }
